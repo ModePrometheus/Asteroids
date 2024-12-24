c77bedcfdade03157d24716d2baf545a5fbf1552
ModePrometheus <wanderinggraphite@gmail.com>
2024-12-24 11:05:53 -0500

    added player.py, modified main.py, modified constants.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -7,6 +8,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     frame_limit = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     # Game Loop
     while True:
@@ -15,6 +17,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         # limit framerate to 60 fps
         dt = frame_limit.tick(60) / 1000
