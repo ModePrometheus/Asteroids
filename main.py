c1a543e15463666096de5bc29c9218f115709400
ModePrometheus <wanderinggraphite@gmail.com>
2024-12-24 10:31:03 -0500

    add circleshap.py

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from constants import *
 
 
 def main():
+    # initialization before game loop
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    frame_limit = pygame.time.Clock()
+    dt = 0
+    # Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -13,6 +16,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        # limit framerate to 60 fps
+        dt = frame_limit.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
