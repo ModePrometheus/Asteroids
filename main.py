e864a765606e81c705119207edf8545793e44a7c
ModePrometheus <wanderinggraphite@gmail.com>
2026-02-20 21:58:57 -0500

    Add Player Movement and Asteroid Field

--- main.py
+++ main.py
@@ -2,14 +2,32 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    #Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    #Containers
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    #Objects
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
+
     dt = 0
 
+    #Game Loop
     while True:
         log_state()
 
@@ -17,13 +35,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
+        # Draw to screen
         screen.fill("black")
-        player.draw(screen)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
-        
-        #Limit Framerate to 60 FPS
+
+        # Limit Framerate to 60 FPS
         dt = clock.tick(60) / 1000
-            
 
 
 if __name__ == "__main__":
